bff708601c876531d8b6b18ffab5491c5cb29da6
Jeffrey Boudreaux <jaboudreauxjr@gmail.com>
2018-04-25 14:04:06 -0500

    Merge branch 'master' of https://github.com/jprodogy/MoodApp into jeff-dev
    # Conflicts:
    #	app/src/main/res/values/dimens.xml
    #	app/src/main/res/values/styles.xml

--- app/src/main/java/com/example/jeffreyboudreaux/moodapp/MainActivity.java
+++ app/src/main/java/com/example/jeffreyboudreaux/moodapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.jeffreyboudreaux.moodapp;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -14,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+        BottomNavigationView toolbar = (BottomNavigationView) findViewById(R.id.navigationView);
+        
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
