e81fd225329bf06d6de0bb1b1b0637908daafede
Anjuli J <anjuli@Khoranhlais-MBP.xavier.xula.local>
2018-04-25 14:19:10 -0500

    menu

--- app/src/main/java/com/example/jeffreyboudreaux/moodapp/MainActivity.java
+++ app/src/main/java/com/example/jeffreyboudreaux/moodapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.jeffreyboudreaux.moodapp;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,19 +11,42 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.navigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        int id = item.getItemId();
+                        switch (id) {
+                            case R.id.nav_addRecord:
+                                startActivity(new Intent(this, RecordMoodActivity.class));
+                                return true;
+                            case R.id.nav_viewRecords:
+                                startActivity(new Intent(this, MoodHistory.class));
+                                return true;
+                            case R.id.nav_homepage:
+                                startActivity(new Intent(this, MainActivity.class));
+                                return true;
+                        }
+                        return false;
+                    }
+                });
+
+
+        }
+/*         @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            getMenuInflater().inflate(R.menu.nav, menu);
+            return true;
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.nav, menu);
-        return true;
-    }
-    @Override
+   @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -30,19 +55,16 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         switch (id) {
             case R.id.nav_addRecord:
-                Intent insertIntent = new Intent(this, RecordMoodActivity.class);
-                this.startActivity(insertIntent);
+                this.startActivity(new Intent(this, RecordMoodActivity.class));
                 return true;
             case R.id.nav_viewRecords:
-                Intent historyIntent = new Intent(this, MoodHistory.class);
-                this.startActivity(historyIntent);
+                this.startActivity(new Intent(this, MoodHistory.class));
                 return true;
             case R.id.nav_homepage:
-                Intent mainIntent = new Intent(this, MainActivity.class);
-                this.startActivity(mainIntent);
+                this.startActivity(new Intent(this, MainActivity.class));
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
-    }
+    }*/
 }
