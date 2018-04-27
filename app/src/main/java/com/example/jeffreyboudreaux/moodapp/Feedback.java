ee762a1274a5096e960609d7002ff60cbbccc132
Jeffrey Boudreaux <jaboudreauxjr@gmail.com>
2018-04-27 14:30:30 -0500

    Merge branch 'anjuli-dev' into jeff-dev
    # Conflicts:
    #	app/src/main/java/com/example/jeffreyboudreaux/moodapp/Encouragements.java
    #	app/src/main/java/com/example/jeffreyboudreaux/moodapp/Feedback.java
    #	app/src/main/java/com/example/jeffreyboudreaux/moodapp/RecordMoodActivity.java

--- app/src/main/java/com/example/jeffreyboudreaux/moodapp/Feedback.java
+++ app/src/main/java/com/example/jeffreyboudreaux/moodapp/Feedback.java
@@ -3,16 +3,14 @@ package com.example.jeffreyboudreaux.moodapp;
 import android.content.Context;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
-import java.util.Scanner;
 
 public class Feedback {
     private Map<Integer, String[]> feeMap;
@@ -47,6 +45,7 @@ public class Feedback {
 
         }
     }
+
 
     public String getFeed(){
         return feed;
