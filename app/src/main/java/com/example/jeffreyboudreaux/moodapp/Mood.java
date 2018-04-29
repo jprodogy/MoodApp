4cfb69d8bdb09b0a490186d9b46aed5bc22d5614
Jeffrey Boudreaux <jaboudreauxjr@gmail.com>
2018-04-29 08:13:04 -0500

    Merge branch 'master' into jeff-dev
    # Conflicts:
    #	app/src/main/java/com/example/jeffreyboudreaux/moodapp/Encouragements.java

--- app/src/main/java/com/example/jeffreyboudreaux/moodapp/Mood.java
+++ app/src/main/java/com/example/jeffreyboudreaux/moodapp/Mood.java
@@ -3,10 +3,29 @@ package com.example.jeffreyboudreaux.moodapp;
 import java.util.ArrayList;
 
 public class Mood {
-    private ArrayList<Integer> moods;
+    private ArrayList<String> moods;
 
     public void addMood(int i){
-        moods.add(i);
+        if (i == 0){
+            moods.add("Happy");
+        }else if (i == 1){
+            moods.add("Sad");
+        }else if (i == 2){
+            moods.add("Mad");
+        }else if (i == 3){
+            moods.add("Tired");
+        }else if (i == 4){
+            moods.add("Stressed");
+        }else if (i == 5){
+            moods.add("Depressed");
+        }else if (i == 6){
+            moods.add("Nervous");
+        }else if (i == 7){
+            moods.add("Confused");
+        }else if (i == 8){
+            moods.add("Mellow");
+        }
+        
     }
     public ArrayList getMood(){
         return moods;
